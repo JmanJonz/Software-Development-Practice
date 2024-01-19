a06f854cea4f93d90facea5e0ecf96f8b0df578d
Jammonjones <wjssaan@gmail.com>
2024-01-18 19:50:46 -0700

    chat between browsers on local device

--- Web Development Practice/Fullstack Practice/Simple Chat App/backend/server.js
+++ Web Development Practice/Fullstack Practice/Simple Chat App/backend/server.js
@@ -1,12 +1,22 @@
-import {WebSocketServer} from "ws";
-const server = new WebSocketServer({port: "3000"});
+import {createServer} from "http";
+import {Server} from "socket.io";
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
+    }
+})
 
 // once ws connection is established listen for message
-    server.on("connection", socket => {
+    io.on("connection", socket => {
+        console.log(`User ${socket.id.substring(0,5)} connected`)
         console.log("Connection Made");
-        socket.on("message", message => {
-            const b = Buffer.from(message);
-            console.log(b.toString());
-            socket.send(`${message}`);
+        socket.on("message", data => {
+            console.log(data);
+            io.emit("message",`UserId: ${socket.id.substring(0,5)} - ${data}`);
         })
     })
+
+// set up socket.io to listen
+    httpServer.listen(4321, ()=> console.log("Listening On 4321"));
--- Web Development Practice/Fullstack Practice/Simple Chat App/frontend/app.js
+++ Web Development Practice/Fullstack Practice/Simple Chat App/frontend/app.js
@@ -1,20 +1,20 @@
-const socket = new WebSocket("ws://localhost:3000");
+const socket = io("ws://localhost:4321");
 
 function sendMessage(e){
     e.preventDefault();
     const input = document.querySelector("input");
     if(input.value){
-        socket.send(input.value);
+        socket.emit("message", input.value);
         input.value = "";
     }
     input.focus();
 }
 
 document.querySelector("form")
-    .addEventListener("submit", sendMessage);
+    .addEventListener("submit", sendMessage); 
 
 // listent to messages 
-    socket.addEventListener("message", ({data})=>{
+    socket.on("message", (data)=>{
         const li = document.createElement("li")
         li.textContent = data;
         document.querySelector("ul").appendChild(li);
