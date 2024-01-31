6dcc6560c19a5d2a60a13b2be0a4abab92094826
Jammonjones <wjssaan@gmail.com>
2024-01-30 21:26:08 -0700

    have frontend being built with changes

--- Web Development Practice/Fullstack Practice/Chat App From Scratch/backend-server/frontend-server/src/components/pages/ChatApp.jsx
+++ Web Development Practice/Fullstack Practice/Chat App From Scratch/backend-server/frontend-server/src/components/pages/ChatApp.jsx
@@ -2,14 +2,22 @@ import Chats from "../snippets/Chats"
 import styles from './ChatApp.module.css'
 import Message from "../snippets/Message"
 import MessageList from "../snippets/MessageList"
+import { useState } from "react"
 
 export default function ChatApp(){
+    const [chatList, setChatList] = useState([]);
+
+    function updateChatList(message){
+        setChatList(currentChatList => [...currentChatList, message])
+        console.log(chatList)
+    }
+
     return(
         <>
         <main className={styles.main}>
             <Chats/>
-            <Message/>
-            <MessageList/>
+            <Message updateChatListt={updateChatList}/>
+            <MessageList chatList={chatList}/>
         </main>
         </>
     )
--- Web Development Practice/Fullstack Practice/Chat App From Scratch/backend-server/frontend-server/src/components/snippets/Message.jsx
+++ Web Development Practice/Fullstack Practice/Chat App From Scratch/backend-server/frontend-server/src/components/snippets/Message.jsx
@@ -1,15 +1,18 @@
 import styles from './message.module.css';
 
-export default function Message(){
+export default function Message({updateChatListt}){
     function formSubmit(e){
         e.preventDefault();
+        console.log('message submitted')
+        const formData = new FormData(e.target);
+        updateChatListt({'message': formData.get('message')})
     }
     return(
         <>
         <main className={styles.main}>
             <form onSubmit={formSubmit} className='formMSG'>
                 <label>
-                    <input className={`${styles.input} ${styles.inputOnly}`} placeholder='Type Your Message Here' type="text" />
+                    <input id='message' className={`${styles.input} ${styles.inputOnly}`} placeholder='Type Your Message Here' type="text" />
                 </label>
                 <button className={` sendMessage ${styles.input} ${styles.button}`} type='submit'>Send</button>
             </form>
